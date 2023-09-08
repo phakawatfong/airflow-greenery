45ffb9023f120e6e53edf4e8cbe80a72e7303562
phakawatfong <phakawatfong@gmail.com>
2023-09-08 22:49:21 +0700

    add separated main_api_module.py to be a module for every other DAGs

---
+++ dags/api-get-greenery-data-set-dag.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils import timezone
+
+from  script.main_api_module import extract_data_from_api
+
+table_list = ["addresses", "events", "order-items", "orders", "products", "promos", "users"]
+
+default_args = {
+    "owner" : "airflow",
+    "start_date" : timezone.datetime(2023, 9 ,8),
+}
+
+with DAG(
+    dag_id="greenery_kids_test_parsing_argument",
+    default_args=default_args,
+    schedule=None,
+    catchup=False,
+    tags=["DEB", "2023", "greenery"]
+):
+    for table_name in table_list:
+        extract_data_parse_args = PythonOperator(
+            task_id=f"extract_data_parse_args_{table_name}",
+            python_callable=extract_data_from_api,
+            op_kwargs={"tbl" : table_name },
+
+        )
+
+    # Task dependencies
+    extract_data_parse_args
---
+++ dags/script/main_api_module.py
@@ -0,0 +1,42 @@
+import csv
+import configparser
+import os
+
+import requests
+
+# Setup configuration path AND target path.
+def extract_data_from_api(tbl):
+    root_path = os.getcwd()
+    config_path = f"{root_path}/env_conf"
+    pipeline_config_path = f"{config_path}/pipeline.conf"
+
+    DATA_FOLDER = "data"
+    if not os.path.exists(f"{root_path}/{DATA_FOLDER}"):
+        os.mkdir(f"{root_path}/{DATA_FOLDER}")
+
+    # create configObject to read configuration from pipeline.conf file
+    config = configparser.ConfigParser()
+    config.read(pipeline_config_path)
+    host = config.get("api_config", "host")
+    port = config.get("api_config", "port")
+
+    API_URL = f"http://{host}:{port}"
+
+    # table_list = ["addresses", "events", "order-items", "orders", "products", "promos", "users"]
+
+    # for tbl in table_list:
+        # print(f"{API_URL}/{tbl}")
+    response = requests.get(f"{API_URL}/{tbl}")
+    data = response.json()
+
+    with open(f"{DATA_FOLDER}/{tbl}.csv", "w", newline='') as f:
+        writer = csv.writer(f)
+        header = data[0].keys()
+        # print(writer)
+        # print(header)
+        writer.writerow(header)
+
+        for each in data:
+            writer.writerow(each.values())
+
+
