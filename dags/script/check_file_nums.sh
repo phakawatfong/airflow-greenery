3fe30a8a450eaf2788239067547305162c559b0a
phakawatfong <phakawatfong@gmail.com>
2023-09-09 01:58:05 +0700

    add shell script to count number of .csv files that we get from APIs

---
+++ dags/script/check_file_nums.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+check_file_num=$(ls -lrt /opt/airflow/data/* | wc -l)
+
+echo "number of files : ${check_file_num}"
+
+
+if [[ ${check_file_num} == 7 ]]
+then
+    echo "got all the files ---> proceed to next dag"
+else
+    echo "job failed"
+    exit 1
+fi
